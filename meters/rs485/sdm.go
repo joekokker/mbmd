76856da9623a7717bbb0c81b7c05579a2cc2f48e
Joe Kokker <joekokker@epios.eu>
2020-02-29 22:48:25 +0100

    Add new values to SDM630

--- meters/rs485/sdm.go
+++ meters/rs485/sdm.go
@@ -25,15 +25,27 @@ func NewSDMProducer() Producer {
 		VoltageL1:     0x0000,
 		VoltageL2:     0x0002,
 		VoltageL3:     0x0004,
+		VoltageL1toL2: 0x00c8,  // Phase  to  Phase  Voltage
+		VoltageL2toL3: 0x00ca,  // Phase  to  Phase  Voltage
+		VoltageL3toL1: 0x00cc,  // Phase  to  Phase  Voltage
+		VoltageLtoL:   0x00ce,  // Phase  to  Phase  Mean  Voltage
 		CurrentL1:     0x0006,
 		CurrentL2:     0x0008,
 		CurrentL3:     0x000A,
+		CurrentN:      0x00e0,
+		Current:       0x0012,
 		PowerL1:       0x000C,
 		PowerL2:       0x000E,
 		PowerL3:       0x0010,
 		Power:         0x0034,
-		ApparentPower: 0x0038,
-		ReactivePower: 0x003C,
+		ApparentPowerL1:   0x0012,  // Phase Apparent Power
+		ApparentPowerL2:   0x0014,  // Phase Apparent Power
+		ApparentPowerL3:   0x0016,  // Phase Apparent Power
+		ApparentPower:     0x0038,  // Total apparent power
+		ReactivePowerL1    0x0018,  // Phase Reactive Power
+		ReactivePowerL2    0x001a,  // Phase Reactive Power
+		ReactivePowerL3    0x001c,  // Phase Reactive Power
+		ReactivePower:     0x003c,  // Total reactive power
 		ImportPower:   0x0054,
 		ImportL1:      0x015a,
 		ImportL2:      0x015c,
@@ -54,12 +66,16 @@ func NewSDMProducer() Producer {
 		THDL1:         0x00ea, // voltage
 		THDL2:         0x00ec, // voltage
 		THDL3:         0x00ee, // voltage
-		THD:           0x00F8, // voltage
+		THD:           0x00f8, // voltage
 		Frequency:     0x0046,
-		//L1THDCurrent: 0x00F0, // current
-		//L2THDCurrent: 0x00F2, // current
-		//L3THDCurrent: 0x00F4, // current
-		//AvgTHDCurrent: 0x00Fa, // current
+		THDL1Current:      0x00f0,  // Line current, THD 
+		THDL2Current:      0x00f2,  // Line current, THD 
+		THDL3Current:      0x00f4,  // Line current, THD
+		THDCurrent:        0x00fa,  // Line current, Mean THD
+		PowerFactorL1:     0x001e,  // Phase Power Factor (+/-)
+		PowerFactorL2:     0x0020,  // Phase Power Factor (+/-) 
+		PowerFactorL3:     0x0022,  // Phase Power Factor (+/-) 
+		PowerFactor:       0x003e,  // Total Power Factor (+/-) Positive for capacitive and negative for inductive.
 		//ApparentImportPower: 0x0064,
 	}
 	return &SDMProducer{Opcodes: ops}
